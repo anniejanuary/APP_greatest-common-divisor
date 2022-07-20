92ee762715062182d80216cbd8f1ae574c841ece
anniejanuary <109144163+anniejanuary@users.noreply.github.com>
2022-07-20 12:28:11 +0200

    Update greatest-common-divisor.py

--- greatest-common-divisor.py
+++ greatest-common-divisor.py
@@ -11,3 +11,15 @@ while a!=b:
     else:
         b=b-a
 print("The greatest common divisor is: ", a)
+
+
+#   OR with a function:
+
+def greatest_common_divisor(a,b):
+    while a!=b:
+        if a>b:
+            a=a-b
+        else:
+            b=b-a
+    return a
+print(greatest_common_divisor(15,5))
